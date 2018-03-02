2c28c592bfb3ae2fc35943cee6caaed479e3d88b
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-03-02 18:26:47 +0000

    extra lines of code added to cli.py file

--- src/led_tester/Cli.py
+++ src/led_tester/Cli.py
@@ -7,8 +7,15 @@ click.disable_unicode_literals_warning = True
 @click.option("--input", default=None, help="input URI (file or URL)")
 def main(input=None):
     """Console script for LED tester"""
-    click.echo()
+
     print("input", input)
+    N, instructions = parseFile(input)
+    ledtester = LEDtester(N)
+
+    for instruction in instructions:
+        ledtester.apply(instruction)
+
+    print('#occupied: ', ledtester.countOccupied())
     return 0
 
 if __name__ == "__main__":
