871b38bfe59ffa305632ea550556516d1831272d
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-03-09 14:47:35 +0000

    removing cli file as using argparse instead. removing skeleton file

--- src/led_tester/test_led_code.py
+++ src/led_tester/test_led_code.py
@@ -1,4 +1,5 @@
 import sys
+import led_tester
 sys.path.append('.')
 import pytest
 from click.testing import CliRunner
@@ -10,3 +11,9 @@ def test_command_line_interface():
     ifile = "./data/test_data.txt"
     N, instructions = parse.parseFile(ifile)
     assert N is not None
+
+def test_read_file():
+    ifile = "./data/test_data.txt"
+    N, instructions = parse.parseFile(ifile)
+    assert N == 10
+    assert instructions == ['turn on 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'switch 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn off 0,0 through 9,9\n', 'turn on 2,2 through 7,7\n']
