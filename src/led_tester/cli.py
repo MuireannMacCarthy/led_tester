461694e096322aef322a5e24d5318f62b8e796aa
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-03-12 14:45:34 +0000

    pushing changes made to main, cli file and test_data

---
+++ src/led_tester/cli.py
@@ -0,0 +1,23 @@
+"""Console script for LED tester"""
+import sys
+import click
+click.disable_unicode_literals_warning = True
+
+@click.command()
+@click.option("--input", default=None, help="input URI (file or URL)")
+def main(input=None):
+    """Console script for LED tester"""
+    print("input", input)
+    N, instructions = parseFile(input)
+
+    ledTester = LEDTester(N)
+
+    for instruction in instructions:
+        ledTester.apply(instruction)
+
+    print('#occupied: ', ledTester.countOccupied())
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
